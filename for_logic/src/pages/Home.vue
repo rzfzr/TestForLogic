101189d1f1fa4a6d5ee5df699f4e0c59dd8970a0
Rafael Goncalves <capitao.rafa@gmail.com>
2020-06-13 18:03:43 -0300

    Simple routing done

---
+++ for_logic/src/pages/Home.vue
@@ -0,0 +1,18 @@
+  
+<template>
+  <main-layout>
+    <p>Welcome home</p>
+  </main-layout>
+</template>
+
+<script>
+import MainLayout from "../layouts/Main.vue";
+export default {
+  components: {
+    MainLayout
+  }
+};
+</script>
+
+
+
